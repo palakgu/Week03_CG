3df5d3537b7b8ac08c3179244c929a4a587bce7d
palak gupta <palakofficial21@gmail.com>
2025-02-04 12:22:54 +0530

    Palak add Remove duplicate using String Builder

--- Day04/Day04-Stringbuilder_And_StringBuffer/src/main/java/stringbuilder/remove_duplicate/Remove_Duplicate.java
+++ Day04/Day04-Stringbuilder_And_StringBuffer/src/main/java/stringbuilder/remove_duplicate/Remove_Duplicate.java
@@ -1,4 +1,27 @@
 package stringbuilder.remove_duplicate;
 
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class Remove_Duplicate {
+    public static String  removeDuplicate(String str){
+        StringBuilder sb = new StringBuilder();
+        HashSet<Character> set = new HashSet<>();
+        for(char i : str.toCharArray()){
+
+            if(!set.contains(i)){
+                sb.append(i);
+            }
+            set.add(i);
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Enter the String ");
+        String str = sc.nextLine();
+
+        System.out.println(Remove_Duplicate.removeDuplicate(str));
+    }
 }
